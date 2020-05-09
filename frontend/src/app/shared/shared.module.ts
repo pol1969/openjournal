295657e590aeb0974252238ea49d8c3d21be6b76
podberezsky <podberezsky@my.com>
2020-05-09 23:02:10 +0300

    Поставили jquery, uikit, сделали меню

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
+import { LeftNavigationComponent } from './left-navigation/left-navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TopNavigationComponent, LeftNavigationComponent, FooterComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+	  TopNavigationComponent,
+	  LeftNavigationComponent,
+	  FooterComponent
   ]
+
 })
 export class SharedModule { }
