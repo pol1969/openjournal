5f85725b26980b96fa5f9fb69f5ce2fb0819f0c1
podberezsky <podberezsky@my.com>
2020-05-12 08:04:16 +0300

    Добавили компоненты входа и выхода

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -3,18 +3,22 @@ import { CommonModule } from '@angular/common';
 import { TopNavigationComponent } from './top-navigation/top-navigation.component';
 import { LeftNavigationComponent } from './left-navigation/left-navigation.component';
 import { FooterComponent } from './footer/footer.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 
 
 @NgModule({
-  declarations: [TopNavigationComponent, LeftNavigationComponent, FooterComponent],
+  declarations: [TopNavigationComponent, LeftNavigationComponent, FooterComponent, SignUpComponent, SignInComponent],
   imports: [
     CommonModule
   ],
   exports: [
 	  TopNavigationComponent,
 	  LeftNavigationComponent,
-	  FooterComponent
+	  FooterComponent,
+	  SignUpComponent,
+	  SignInComponent
   ]
 
 })
