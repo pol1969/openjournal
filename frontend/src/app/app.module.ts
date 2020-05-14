bd3ba2143e8178392b76c868387d47918a6e0b7c
podberezsky <podberezsky@my.com>
2020-05-14 18:23:42 +0300

    Сделал jumbotron

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/shared.module';
-
+import { Routes, RouterModule } from '@angular/router'; // *new
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module'; // *new 
+import { ProfileModule } from './profile/profile.module'; // *new
+const appRoutes: Routes = [ // *new 
+	{ // *new 
+		path: 'home', // *new 
+		loadChildren: './home/home.module#HomeModule' // *new 
+	}, // *new 
+	{ // *new 
+		path: 'profile', // *new 
+		loadChildren: './profile/profile.module#ProfileModule' // *new 
+	}, // *new 
+	{ // *new 
+		path: '', // *new 
+		redirectTo: '/home', // *new 
+		pathMatch: 'full' // *new 
+	} // *new 
+] // *new
 
 @NgModule({
   declarations: [
@@ -11,6 +28,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     SharedModule,
+    HomeModule, 
+    ProfileModule, 
+    RouterModule.forRoot(appRoutes) // *new
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { JumbotronComponent } from './home/jumbotron/jumbotron.component';
+import { FeaturesComponent } from './home/features/features.component';
+import { Routes, RouterModule } from '@angular/router'; // *new
+
+const appRoutes: Routes = [ // *new 
+	{ // *new 
+		path: 'home', // *new 
+		component: HomeComponent, // *new 
+	} // *new 
+] // *new
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, JumbotronComponent, FeaturesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(appRoutes) // *new
   ]
 })
 export class HomeModule { }
