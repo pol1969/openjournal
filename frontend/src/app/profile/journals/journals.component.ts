208d11b36da3f24d6217ab7c0a6d9092d259e0e4
podberezsky <podberezsky@my.com>
2020-05-23 07:12:59 +0300

    Сделан вывод всех журналов

--- frontend/src/app/profile/journals/journals.component.ts
+++ frontend/src/app/profile/journals/journals.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service' // *new
+
 
 @Component({
-  selector: 'app-journals',
-  templateUrl: './journals.component.html',
-  styleUrls: ['./journals.component.css']
+	selector: 'app-journals',
+	templateUrl: './journals.component.html',
+	styleUrls: ['./journals.component.css']
 })
 export class JournalsComponent implements OnInit {
+	public journals; // *new
 
-  constructor() { }
+	constructor(
+		private profileService: ProfileService // *new
+	) { }
 
-  ngOnInit(): void {
-  }
+	// *new
+	getAllJournals() {
+		this.profileService.getJournals().subscribe(
+			(res) => {
+				console.log("test from getAllJournals")
+				console.log(res);
+				this.journals = res
+			},
+			(err) => {
+				console.log(err);
+			}
+		);
+	}
+
+
+
+	ngOnInit(): void {
+		this.getAllJournals();
+	}
 
 }
--- frontend/src/app/profile/profile.service.ts
+++ frontend/src/app/profile/profile.service.ts
@@ -3,21 +3,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ProfileService {
 	DJANGO_SERVER = 'http://127.0.0.1:8000'
 
 
 
-  constructor(
-  	private http: HttpClient //new
-  ) { }
+	constructor(
+		private http: HttpClient //new
+	) { }
 
-  createJournal(formData: any) { //new
-    console.log(formData);
-    return this.http.post(
-      this.DJANGO_SERVER + '/create-journal', formData
-    );
-  }
+	createJournal(formData: any) { //new
+		console.log(formData);
+		return this.http.post(
+			this.DJANGO_SERVER + '/create-journal', formData
+		);
+	}
+	getJournals() {
+		return this.http.get(
+			this.DJANGO_SERVER + '/get-journals'
+		);
+	}
 }
